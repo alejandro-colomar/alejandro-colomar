2641a7095857cd6b54cb6d3edaecf21760a9e39d
Alejandro Colomar <colomar.6.4.3@gmail.com>
2020-10-04 11:41:14 +0200

    Use official docker installation script
    Signed-off-by: Alejandro Colomar <colomar.6.4.3@gmail.com>

--- lib/server/install/docker.sh
+++ lib/server/install/docker.sh
@@ -17,78 +17,18 @@
 ################################################################################
 ##	functions							      ##
 ################################################################################
-function dk_uninstall_old_versions__()
+function install_docker()
 {
 
-	apt-get purge -y						\
+	apt-get purge --yes						\
 			docker						\
 			docker-engine					\
 			docker.io					\
 			containerd					\
 			runc;
-}
 
-function dk_prepare_https__()
-{
-
-	apt-get update							&& \
-	apt-get install -y						\
-			apt-transport-https				\
-			ca-certificates					\
-			curl						\
-			gnupg-agent					\
-			software-properties-common;
-}
-
-function dk_add_docker_gpg_key__()
-{
-
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg |apt-key add - && \
-	apt-key fingerprint 0EBFCD88;
-}
-
-function dk_add_docker_repository__()
-{
-
-	add-apt-repository						\
-			"deb https://download.docker.com/linux/ubuntu	\
-			$(lsb_release -cs)				\
-			stable";
-}
-
-function dk_set_up_repository__()
-{
-
-	dk_prepare_https__						&& \
-	dk_add_docker_gpg_key__						&& \
-	dk_add_docker_repository__;
-}
-
-function dk_install_docker_engine__()
-{
-
-	apt-get update							&& \
-	apt-get install -y						\
-			docker-ce					\
-			docker-ce-cli					\
-			containerd.io					&& \
-	## Test the installation
-	docker run --rm hello-world;
-}
-
-function dk_add_user_to_docker_group__()
-{
-
+	curl https://get.docker.com | bash				&& \
 	usermod -a -G docker ubuntu;
-}
-
-function install_docker()
-{
-
-	dk_uninstall_old_versions__;
-	dk_set_up_repository__						&& \
-	dk_install_docker_engine__					&& \
-	dk_add_user_to_docker_group__;
 }
 
 

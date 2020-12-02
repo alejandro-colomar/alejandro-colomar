795a52389feea4ab128a3c3b179829254f670112
Alejandro Colomar <colomar.6.4.3@gmail.com>
2020-12-02 18:56:08 +0100

    git_pull.sh: Minor improvements
    Signed-off-by: Alejandro Colomar <colomar.6.4.3@gmail.com>

--- bin/git_pull.sh
+++ bin/git_pull.sh
@@ -41,9 +41,9 @@ function main()
 		echo "	SSH	${remote}";
 		ssh -n ${remote} "
 			cd /usr/local/src/server/;
-			echo '	GIT	fetch';
-			git fetch;
-			echo '	GIT	checkout';
+			echo '	GIT	fetch -p';
+			git fetch -p;
+			echo '	GIT	checkout	origin/main';
 			git checkout origin/main;
 		";
 	done
